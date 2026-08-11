fa4f07e25d5f5d38be8265e7da5e963bbcfa28f8
NHeroOP <79276833+NHeroOP@users.noreply.github.com>
2026-08-11 22:18:27 +0530

    feat: update socket server URL to use environment variable

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,7 +2,7 @@
 
 import { io } from "socket.io-client";
 
-const socketServerURL = "http://localhost:5000";
+const socketServerURL = process.env.SOCKET_SERVER_URL || "http://localhost:5000";
 
 export const socket = io(socketServerURL, {
   autoConnect: false,
