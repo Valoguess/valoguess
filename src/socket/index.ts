50b316eedbfb26ebcecaadff7bdf2abe4f0a3fad
NHeroOP <79276833+NHeroOP@users.noreply.github.com>
2026-08-11 22:41:45 +0530

    feat: log socket server URL for debugging purposes

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -3,6 +3,7 @@
 import { io } from "socket.io-client";
 
 const socketServerURL = process.env.SOCKET_SERVER_URL || "http://localhost:5000";
+console.log(socketServerURL);
 
 export const socket = io(socketServerURL, {
   autoConnect: false,
