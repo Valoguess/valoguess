ab1e213eba5f3fd33f9fcde6dbbf50509b346666
NHeroOP <79276833+NHeroOP@users.noreply.github.com>
2026-09-20 23:33:09 +0530

    feat: implement authentication callback for socket connection

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,10 +1,24 @@
 "use client";
 
 import { io } from "socket.io-client";
+import { token } from "@/lib/auth-client"
 
 const socketServerURL = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "http://localhost:5000";
 
 export const socket = io(socketServerURL, {
   autoConnect: false,
   transports: ["websocket"],
+
+  auth: async (cb) => {
+    const { data, error } = await token()
+
+    if (error || !data) {
+      cb({});
+      return;
+    }
+
+    cb({
+      token: data.token,
+    });
+  }
 });
