639c575d51dc3de015aae9ddae745dfec569debc
NHeroOP <79276833+NHeroOP@users.noreply.github.com>
2026-09-21 12:58:16 +0530

    feat: update database configuration to use environment variables for credentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
   schema: './src/db/index.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    // url: process.env.DATABASE_URL!,
+    host: process.env.DATABASE_HOST!,
+    port: Number(process.env.DATABASE_PORT!),
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
+    database:  process.env.DATABASE_NAME!,
+    ssl: true
   },
 });
